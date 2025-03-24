d4c90428152ed229bdb164805ac7dbebdf18a5ae
marcelagomees <marcellagomes609@gmail.com>
2025-03-24 16:03:37 -0300

    coisas novas

--- LojaDaMarcela/Controllers/CategoriasController.cs
+++ LojaDaMarcela/Controllers/CategoriasController.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Runtime.InteropServices.JavaScript;
 using System.Threading.Tasks;
 using LojaDaMarcela.Data;
 using LojaDaMarcela.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+
 
 namespace LojaDaMarcela.Controllers
 {
@@ -18,9 +14,8 @@ namespace LojaDaMarcela.Controllers
         private readonly IWebHostEnvironment _host;
 
         public CategoriasController(
-            ILogger<CategoriasController> logger,
-             AppDbContext db, IWebHostEnvironment host
-            )
+            ILogger<CategoriasController> logger, AppDbContext db, IWebHostEnvironment host
+        )
         {
             _logger = logger;
             _db = db;
@@ -28,11 +23,13 @@ namespace LojaDaMarcela.Controllers
         }
 
         public IActionResult Index()
-        { 
+        {
             List<Categoria> categorias = _db.Categorias.ToList();
             return View(categorias);
         }
 
+
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
@@ -40,17 +37,18 @@ namespace LojaDaMarcela.Controllers
 
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(Categoria categoria, IFormFile Arquivo) {
-            if(ModelState.IsValid)
+        public async Task<IActionResult> Create(Categoria categoria, IFormFile Arquivo)
+        {
+            if (ModelState.IsValid)
             {
                 _db.Categorias.Add(categoria);
                 await _db.SaveChangesAsync();
 
-                //Salvar a foto no servidor
-                if (Arquivo !=null)
+                // Salvar a foto no servidor
+                if (Arquivo != null)
                 {
                     string nomeArquivo = categoria.Id + Path.GetExtension(Arquivo.FileName);
-                    string caminho = Path.Combine(_host.WebRootPath,"img\\categorias");
+                    string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
                     string novoArquivo = Path.Combine(caminho, nomeArquivo);
                     using (FileStream stream = new(novoArquivo, FileMode.Create))
                     {
@@ -59,13 +57,96 @@ namespace LojaDaMarcela.Controllers
                     categoria.Foto = "\\img\\categorias\\" + nomeArquivo;
                     await _db.SaveChangesAsync();
                 }
-                TempData["Success"] = "Categoria cadastrada com sucesso!";
+                TempData["success"] = "Categoria cadastrada com sucesso!";
                 return RedirectToAction(nameof(Index));
-
             }
-            return View();
+
+            return View(categoria);
         }
-        
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var categoria = await _db.Categorias
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (categoria == null)
+                return NotFound();
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var categoria = await _db.Categorias
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (categoria == null)
+                return NotFound();
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Categoria categoria, IFormFile Arquivo)
+        {
+            if (id != categoria.Id)
+                return NotFound();
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    if (Arquivo != null)
+                    {
+                        string nomeArquivo = categoria.Id + Path.GetExtension(Arquivo.FileName);
+                        string caminho = Path.Combine(_host.WebRootPath, "img\\categorias");
+                        string novoArquivo = Path.Combine(caminho, nomeArquivo);
+                        using (FileStream stream = new(novoArquivo, FileMode.Create))
+                        {
+                            Arquivo.CopyTo(stream);
+                        }
+                        categoria.Foto = "\\img\\categorias\\" + nomeArquivo;
+                    }
+                    _db.Categorias.Update(categoria);
+                    await _db.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    return NotFound();
+                }
+                
+                TempData["success"] = "Categoria alterada com sucesso!";
+                return RedirectToAction(nameof(Index));
+            }
+            return View(categoria);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+                return NotFound();
+            var categoria = await _db.Categorias
+                .FirstOrDefaultAsync(c => c.Id == id);
+            if (categoria == null)
+                return NotFound();
+            return View(categoria);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+            public async Task<IActionResult> Delete (int id)
+            {
+                var categoria = await _db.Categorias.FindAsync(id);
+                if (categoria != null)
+                    _db.Categorias.Remove(categoria);
+                await _db.SaveChangesAsync();
+                TempData ["Success"] = "Categoria Excluida com Sucesso";
+                return RedirectToAction(nameof(Index));
+            }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
